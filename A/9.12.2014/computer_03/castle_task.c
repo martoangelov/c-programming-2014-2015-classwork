6b914bd9f3179729851a224971269f6f093aa53e
Vasil Bozhurski <slightremorse@gmail.com>
2014-12-09 18:55:08 +0200

    castle

--- A/9.12.2014/computer_03/castle_task.c
+++ A/9.12.2014/computer_03/castle_task.c
@@ -1,23 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-	int random_number;
-	int i;
+int main(){	
+	int mapLength,i;
+	
+	scanf("%d",&mapLength);
+	
+	int gameMap[mapLength][mapLength];
 	
 	srand(time(NULL));
 	
-	for(i = 0; i < 3; i++)
-	{
-	random_number = rand() % 9 + 1;
-	printf("%d\n", random_number);
+	for(i = 0; i < mapLength; i++){
+		gameMap[0][i] = rand() % 9 + 1;
 	}
+	for(i = 1; i < (mapLength - 1)){
 	
+	}
 		
-	printf("%d\n", time(NULL));	
 	
 
-return 0;
+	return 0;
 }
+//5 8 9 4 3
+//9       4
+//6       7
+//4       9
+//8 7 2 3 5
